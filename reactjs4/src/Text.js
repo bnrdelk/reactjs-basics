730063751704ed20d017b9bb140298cab11c1132
bnrdelk <bnrdelk@icloud.com>
2024-02-10 20:34:18 +0300

    useEffect hook, lifecycle

---
+++ reactjs4/src/Text.js
@@ -0,0 +1,23 @@
+import {useState, useEffect} from 'react';
+
+export const Text = () => {
+const [text, setText] = useState("");
+
+useEffect(() => {
+    console.log("Component Mounted");
+
+    return () => {
+        console.log("Component Unmounted");
+    }
+}, []);
+ 
+  return (
+    <div className="Text">
+        <input onChange={(event) => {
+            setText(event.target.value);
+        }}></input>
+    <h1> {text} </h1>
+    </div>
+  );
+
+}

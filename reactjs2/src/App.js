d613509e01eec366186e03d642f4909c5d46ca0e
bnrdelk <bnrdelk@icloud.com>
2024-02-10 11:50:08 +0300

    useState hook, counter added

--- reactjs2/src/App.js
+++ reactjs2/src/App.js
@@ -2,14 +2,10 @@ import './App.css';
 import { useState } from "react";
 
 function App() {
-const [age, setAge] = useState(0);
 const [inputValue, setInputValue] = useState("");
 const [showText, setShowText] = useState(true);
 const [textColor, setTextColor] = useState("red");
-
-const increaseAge = () => {
- setAge(age+1);
-};
+const [count, setCount] = useState(0);
 
 const handleChange = (event) => {
   setInputValue(event.target.value);
@@ -19,22 +15,36 @@ const handleChange = (event) => {
   setShowText(!showText);
  };
 
+const increaseCount = () => {
+ setCount(count + 1);
+};
+
+const decreaseCount = () => {
+  setCount(count - 1);
+};
+
+const resetCount = () => {
+  setCount(0);
+};
+
   return (
     <div className="App">
       <button onClick = {showHideText}> Show or Hide Text </button>
       {showText && <h1> Show or Hide the text</h1>}
       
       <button onClick = {() => {
-        setTextColor(textColor==="red" ? "green" : "red")}}
-        >
-           Change Color </button>
+        setTextColor(textColor==="red" ? "green" : "red")}}> Change Color </button>
       <h1 style={{color: textColor}}> Change the Text Color </h1>
-
-      {age}
-      <button onClick = {increaseAge}> Increase Age </button>
 
       <input type = "text" onChange = {handleChange}></input>
       {inputValue}
+
+      {count}
+      
+      <button onClick = {increaseCount}> Increase </button>
+      <button onClick = {decreaseCount}> Decrease </button>
+      <button onClick = {resetCount}> Reset </button>
+     
     </div>
   );
 }
